ffac9e1e28eb71da0c93ab7404221e1c250ddf5c
Sergei Shmanko <admin@phoenix.ee>
2016-05-24 09:01:00 +0300

    change to working dir before launch

--- webapp/recipes/default.rb
+++ webapp/recipes/default.rb
@@ -41,7 +41,7 @@ template "#{node[:webapp][:base_dir]}/webapp-master/conf/webapp.cfg" do
 end
 
 execute 'Run webapp' do
-  command "python #{node[:webapp][:base_dir]}/webapp-master/webapp.py &> #{node[:webapp][:base_dir]}/webapp-master/log/webapp.log &"
+  command "cd #{node[:webapp][:base_dir]}/webapp-master; python webapp.py &> log/webapp.log &"
 end
 
 include_recipe 'webapp::tests'
